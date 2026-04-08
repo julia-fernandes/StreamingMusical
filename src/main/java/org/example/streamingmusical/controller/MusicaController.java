bd224f34883e8b0fcb97f6ac23cb7c17785cc31d
juliac-jpg <juliafcostcds@gmail.com>
2026-04-07 21:35:11 -0300

    create controller

---
+++ src/main/java/org/example/streamingmusical/controller/MusicaController.java
@@ -0,0 +1,51 @@
+package org.example.streamingmusical.controller;
+
+import jakarta.validation.Valid;
+import org.example.streamingmusical.model.Musica;
+import org.example.streamingmusical.service.MusicaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/streaming")
+public class MusicaController {
+
+    private final MusicaService service;
+
+    public MusicaController(MusicaService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Musica> listarMusicas(){
+        return service.listarMusicas();
+    }
+
+    @GetMapping("/{id}")
+    public Musica buscarId(@PathVariable Long id){
+        return service.buscarId(id);
+    }
+
+    @GetMapping("/buscar")
+    public List<Musica> buscarNome(String nome){
+        return service.buscarMusica(nome);
+    }
+
+    @PostMapping
+    public Musica salvarMusica(@Valid @RequestBody Musica musica){
+        return service.salvarMusica(musica);
+    }
+
+    @PutMapping("/{id}")
+    public Musica alterarMusica(@PathVariable @Valid @RequestBody Long id, Musica musica){
+        return service.alterarMusica(id, musica);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarMusica(@PathVariable Long id){
+        service.deletarMusica(id);
+    }
+}
